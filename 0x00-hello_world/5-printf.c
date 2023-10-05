10ef648d18a10a478c04d603d894008ab93260e0
HajarElMannani <laso125@hotmail.com>
2023-10-05 13:35:58 -0700

    print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+*main- Entry point
+*Return: Always 0 when success
+*/
+int main(void)
+{
+  printf("With proper grammar, but the outcome is a piece of art,");
+  return (0);
+}
