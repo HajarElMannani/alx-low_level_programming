6bd73cb130585ddd61013e826ec077ab0c38811c
HajarElMannani <laso125@hotmail.com>
2023-10-05 13:12:39 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 /**
 *main- prints "Programming is lie building a multilingual puzzle
+return=0 if the function main is exited
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 /**
-*return 0 if the function is exited succesfully
+*return=0 if the function is exited succesfully
 */
 return (0);
 }
