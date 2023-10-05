8c5f61a629c80a87d743c53ce739f5f36aa9172c
HajarElMannani <laso125@hotmail.com>
2023-10-05 12:45:09 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-/**main- prints "Programming is lie building a multilingual puzzle*/
 
+/*main- prints "Programming is lie building a multilingual puzzle*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
