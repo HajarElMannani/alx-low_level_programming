9237ee10c0a592ec54b5435f9ad33c8479fd472d
HajarElMannani <laso125@hotmail.com>
2023-10-05 12:57:42 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
-
 /**
- *main- prints "Programming is lie building a multilingual puzzle
- *return=0 if the function is well exited 
+*main- prints "Programming is lie building a multilingual puzzle
+*return=0 if the function is well exited 
 */
 int main(void)
 {
