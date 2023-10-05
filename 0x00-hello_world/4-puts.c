c469370c34813fd636a61b18c4757415fca7e752
HajarElMannani <laso125@hotmail.com>
2023-10-05 13:09:11 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,8 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-/*return 0 if the function is exited succesfully*/
+/**
+*return 0 if the function is exited succesfully
+*/
 return (0);
 }
