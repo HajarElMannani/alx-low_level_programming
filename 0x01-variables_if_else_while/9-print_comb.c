0cf104893d5be10d6e558a6bb65526ad432a9522
HajarElMannani <laso125@hotmail.com>
2023-10-06 14:18:16 -0700

    Print conbination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ *main-Start of the program
+ *Return: 0 when success
+ */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+putchar(',');
+putchar(' ');
+putchar('\n');
+}
+return (0);
+}
