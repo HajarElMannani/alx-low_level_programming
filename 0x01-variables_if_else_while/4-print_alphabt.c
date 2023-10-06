9c8b576fc92d1387cd039c92941061becfe921fe
HajarElMannani <laso125@hotmail.com>
2023-10-06 12:23:59 -0700

    Print alphaber with exeptions

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,7 +6,9 @@
 int main(void)
 {
 char n;
-for (n = 'a'; n <= 'z' && n != 'q' && n != 'e'; n++)
+for (n = 'a'; n <= 'z'; n++)
+if (n == 'e' || n == 'q')
+n++;
 putchar(n);
 putchar('\n');
 return (0);
