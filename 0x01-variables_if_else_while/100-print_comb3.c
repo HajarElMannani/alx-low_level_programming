e261def1bd1021b5a4d6a500a990be00e1bc056f
HajarElMannani <laso125@hotmail.com>
2023-10-07 10:53:41 -0700

    Print combination of 2 numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,15 +5,20 @@
  */
 int main(void)
 {
-int n;
+  int n, i;
 for (n = '0'; n <= '8'; n++)
 {
+while (i < n)
+{
 putchar(n);
-putchar(n + 1);
+i = n + 1;
+putchar(i);
+i++;
 if (n < 8)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
