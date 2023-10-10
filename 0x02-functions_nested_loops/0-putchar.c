e39a991e169cb51dd083070ab5d6ec349fc73562
HajarElMannani <laso125@hotmail.com>
2023-10-10 06:13:35 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ *main- This program prints _putchar
+ *_putchar: write character to print
+ *@char: type of argument
+ *Return: 0 when success
+ */
+int _putchar(char);
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
