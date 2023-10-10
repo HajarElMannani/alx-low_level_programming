9904d477ad8b49a77e0d5461437d9f0e0f2e59a2
HajarElMannani <laso125@hotmail.com>
2023-10-10 12:24:13 -0700

    Print clock

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,13 @@
 void jack_bauer(void)
 {
 char i, j;
-for (i = '0'; i <= '23'; i++)
+for (i = 0; i <= 23; i++)
 {
-for (j = '0'; j <= '59'; j++)
+for (j = 0; j <= 59; j++)
 {
-_putchar(i);
+_putchar(i - '0');
 _putchar(':');
-_putchar(j);
+_putchar(j - '0');
 _putchar('\n');
 }
 }
