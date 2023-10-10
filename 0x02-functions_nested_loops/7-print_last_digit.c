2eb1dca3e40911eb7287cd0e5f85cf3d1ab7219a
HajarElMannani <laso125@hotmail.com>
2023-10-10 10:08:48 -0700

    Return last digit of number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,11 @@
 /**
  *print_last_digit - print last digit of a given number
  *@n : number to give last digit of
- *Return: last digit of number
+ *Return:value of last digit of number
  */
 
 int print_last_digit(int n)
 {
 n = n % 10;
-return (n);
+return (nn);
 }
