2329aee594dbcfaab1e4640781eef6af90c6e180
HajarElMannani <laso125@hotmail.com>
2023-10-10 10:21:27 -0700

    Return last digit of number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,14 @@ if (n < 0)
 {
 n = n * -1;
 n = n % 10;
-return (n);
-return (n);
+_putchar('n');
+_putchar('n');
 }
 if (n > 0)
 {
 n = n % 10;
-return (n);
-return (n);
+_putchar('n');
+_putchar('n');
 }
 else
 return (0);
