254c63ea0eb4935c68f86c344d148d917bdfaa59
HajarElMannani <laso125@hotmail.com>
2023-10-10 08:05:56 -0700

    Tell if character is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ *_islower- Checks for lowercase characters
+ *@c: returns 1 if lowercase, 0 if uppercase
+ */
+int _islower(int c)
+{
+char r;
+if ('r' <= 97 && 'r' <= 122)
+return (1);
+else
+return (0);
+}
