ce0b6f90174ca38ef52ce83743b8aa49879388f1
HajarElMannani <laso125@hotmail.com>
2023-10-12 08:51:56 -0700

    First commit mote numbers task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *more_numbers - prints 10 times numbers from 0 to 14
+ *@void: takes no parameter
+ *Return: Numbers from 0 to 14, 10 times
+ */
+void more_numbers(void)
+{
+int i, n;
+for (i = 0; i < 10; i++)
+{
+for (n = 0; n <= 14; n++)
+{
+_putchar(n + '0');
+}
+_putchar('\n');
+}
+}
