76a6eb8e35a77c23eba758b8dd0567d7a46aac2f
HajarElMannani <laso125@hotmail.com>
2023-10-16 11:38:23 -0700

    Set string length

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,16 +9,16 @@ void rev_string(char *s)
 int i, len, j, n;
 char c;
 i = 0;
-n = 0; 
+n = 0;
 while (s[i] != '\0')
 {
 i++;
 }
 len = i;
-for (j = len - 1 ;j >= len / 2; j--)
+for (j = len - 1 ; j >= len / 2; j--)
 {
 c = s[j];
 s[j] = s[n];
-s[n++] = c; 
+s[n++] = c;
 }
 }
