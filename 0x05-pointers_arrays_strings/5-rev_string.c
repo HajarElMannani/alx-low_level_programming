c94bdba2dc2051fab8522b7374a4d8c27bb02abf
HajarElMannani <laso125@hotmail.com>
2023-10-16 10:46:52 -0700

    First commit reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *rev_string - Reverse a string
+ *@s: String to reverse
+ *Return: Nothing
+ */
+void rev_string(char *s)
+{
+int i, len, j, c;
+char l[];
+i = 0;
+n = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+len = i;
+for (j = len - 1; j >= 0; j--)
+{
+c = s[j];
+l[n] = c;
+n++;
+}
+s = l;
+}
