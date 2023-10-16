cf010cc249c7c072bbbea30714440485e631f555
HajarElMannani <laso125@hotmail.com>
2023-10-16 14:08:32 -0700

    Solve error puts_half

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - print n elements of an array of integers
+ *@a: Array of integers
+ *@n: Number of integers to print
+ *Return: Nothing
+ */
+void print_array(int *a, int n)
+{
+int i;
+printf("%d", a[[0]);
+for (i = 1; i <= n; i++)
+{
+printf(", %d", a[i]);
+}
+_putchar('\n');
+}
