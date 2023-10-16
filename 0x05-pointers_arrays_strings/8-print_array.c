056a470e5fd0b85e5144883203641ad7cba09944
HajarElMannani <laso125@hotmail.com>
2023-10-16 14:24:14 -0700

    Solve error print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,20 +8,11 @@
  */
 void print_array(int *a, int n)
 {
-int i, j, len;
+int i;
 printf("%d", a[0]);
-j = 0;
-while (a[j] != '\0')
-{
-j++;
-}
-len = j;
-if (n <= len)
-{
 for (i = 1; i < n; i++)
 {
 printf(", %d", a[i]);
 }
-}
-_putchar('\n');
+printf("\n");
 }
