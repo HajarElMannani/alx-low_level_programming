9ff4f962db568808529de5bd9566cdafb98cfe0d
HajarElMannani <laso125@hotmail.com>
2023-10-17 05:59:31 -0700

    First commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+*print_times_table - print the n times table, starting with 0
+*@n: n times table
+*Return: Nothing
+*/
+int i, j, p;
+if (n >= 0 && n <= 15)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+p = i * j;
+if(p >= 0 && p <= 9)
+{
+putchar(p + '0');
+putchar(',');
+putchar(' ');
+putchar(' ');
+putchar(' ');
+}
+else if (p >= 10 && p <= 99)
+{
+putchar(p + '0');
+putchar(',');
+putchar(' ');
+putchar(' ');
+}
+else
+{
+putchar(p + '0');
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
