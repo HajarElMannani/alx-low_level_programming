ac74afb881528069705451977d9f2ff6efed0cc6
HajarElMannani <laso125@hotmail.com>
2023-10-18 12:44:01 -0700

    Fix error

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- *string_toupper - Change all lowercase letters of a string to uppercase
+ *string_toupper - Change all lowercase 
+ *letters of a string to uppercase
  *@a: string to convert
  *Return: Nothing
  */
