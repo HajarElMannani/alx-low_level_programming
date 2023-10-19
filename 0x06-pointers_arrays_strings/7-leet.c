7ef1aa7b09089703d14299382ef43e054bcec592
HajarElMannani <laso125@hotmail.com>
2023-10-19 02:12:13 -0700

    First commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *leet - Encode a string into 1337
+ *@a: string to encode
+ *Return: string a after encoding
+ */
+char *leet(char *a)
+{
+int i, j;
+char al = "aAeEoOtTlL";
+char num = "4433007711";
+i = 0;
+j = 0;
+while (a[i] != '\0')
+{
+for (j = 0; j < 10; j++)
+{
+if (a[i] == al[j])
+a[i] = num[j];
+}
+i++;
+}
+return (a);
+}
