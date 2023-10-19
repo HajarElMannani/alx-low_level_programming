24ef006b93c1cf862222b5046eca3dc7100c3736
HajarElMannani <laso125@hotmail.com>
2023-10-19 03:50:22 -0700

    Fix error

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,17 +7,16 @@
 char *rot13(char *a)
 {
 int i, j;
-char s1[] = "abcdefghijklmABCDEFGHIJKLMnopqrstuvwxyzNOPQRSTUVWXYZ";
-char s2[] = "nopqrstuvwxyzNOPQRSTUVWXYZabcdefghijklmABCDEFGHIJKLM";
+char *s1 = "abcdefghijklmABCDEFGHIJKLMnopqrstuvwxyzNOPQRSTUVWXYZ";
+char *s2 = "nopqrstuvwxyzNOPQRSTUVWXYZabcdefghijklmABCDEFGHIJKLM";
 i = 0;
 while (a[i] != '\0')
 {
 for (j = 0; j < 52; j++)
 {
 if (a[i] == s1[j])
-{
 a[i] = s2[j];
-}
+break;
 }
 i++;
 }
