2b486424ad13bc80a59ba77ab7f9011b6df073a8
HajarElMannani <laso125@hotmail.com>
2023-10-23 12:28:40 -0700

    fix error

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,13 +3,13 @@
  *_strchr - locates a character in a string
  *@s: string to look in
  *@c: Character to look for
- *Return: pointer to first occurrence
+ *Return: pointer to first occurrence of c
  */
 char *_strchr(char *s, char c)
 {
 int i;
 i = 0;
-while (s[i] != c && s[i] != '\0')
+while ((s[i] != c) && (s[i] != '\0'))
 {
 i++;
 }
