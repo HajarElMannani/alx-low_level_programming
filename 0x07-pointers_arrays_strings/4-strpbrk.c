f1788fe5586507b7f00959277dfe2451816323a5
HajarElMannani <laso125@hotmail.com>
2023-10-23 14:16:22 -0700

    fix error

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,11 +12,11 @@ char *_strpbrk(char *s, char *accept)
 int i, j;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; (accept[j] != '\0') && (accept[j] != s[i]); j++)
-;
+for (j = 0; accept[j] != '\0'; j++)
+{
 if (s[i] == accept[j])
 return (s + i);
-else
-return (NULL);
 }
+}
+return (NULL);
 }
