573a273e1dc7359c8fbbe165bdaf03037f744cf7
HajarElMannani <laso125@hotmail.com>
2023-10-23 08:50:30 -0700

    First commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,10 +10,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 i = 0;
-while (i < n && *s != '\0')
+while (i < n && s[i] != '\0')
 {
-*s = b;
-*s++;
+s[i] = b;
+i++;
 }
 return (s);
 }
