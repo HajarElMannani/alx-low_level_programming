c16f33756a3eb4ba11f04e3e5ef63263cae6933e
HajarElMannani <laso125@hotmail.com>
2023-10-23 08:41:30 -0700

    First commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 /**
  *_memset - fills memory with a constant byte
  *@s: pointer tomemory area
- *@b: constatnt byte 
+ *@b: constatnt byte
  *@n: numberof bytes to replace
  *Return: pointer to the memory area s
  */
@@ -10,10 +10,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 int i;
 i = 0;
-while (i < n && s[i] != '\0')
+while (i < n && *s != '\0')
 {
-s[i] = b;
-i++;
+*s = b;
+*s++;
 }
 return (s);
 }
