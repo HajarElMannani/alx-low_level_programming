f91ab55704b08f0b5f757aaa63a984c6fcb6d720
HajarElMannani <laso125@hotmail.com>
2023-10-23 15:07:28 -0700

    fix error

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,14 +9,13 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-while (*haystack != '\0')
+for (; *haystack != '\0'; haystack++)
 {
 while (*haystack == *needle && *needle != '\0')
 {
 haystack++;
 needle++;
 }
-haystack++; 
 if (*needle == '\0')
 return (haystack);
 }
