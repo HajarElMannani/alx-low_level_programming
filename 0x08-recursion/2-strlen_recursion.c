cc9edb970d9439459c61e87bbad0580757f915ae
HajarElMannani <laso125@hotmail.com>
2023-10-25 09:53:55 -0700

    Fix error

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-int i;
-i = 0;
-if (*s != '\0')
+if (*s == '\0')
 {
-i++;
-_strlen_recursion(s + 1);
-return (i);
+return;
+}
+else
+{
+return (1 + _strlen_recursion(s + 1));
 }
 }
