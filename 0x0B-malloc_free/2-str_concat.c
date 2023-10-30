71f1e0bb6749d2194ec620d84ff7e6727f9aacae
HajarElMannani <laso125@hotmail.com>
2023-10-30 13:00:10 -0700

    First commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,28 +8,42 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-  int j, i;
-  char *t;
-  if (!s1 && !s2)
-    return (NULL);
-  while (*s1++)
-    {
-    i++;
-    }
-  while (*s2++)
-    {
-      j++;
-    }
-  t = malloc(i + j + 1);
-  t = s1;
-  while (*s1)
-    {
-      *t++ = *s1++;
-    }
-  while (*s2)
-    {
-      *t++ = *s2++;
-    }
-  *t = 0;
-  return (t);
+int j, i;
+char *t, *m;
+j = 0;
+i = 0;
+t = s1;
+if (s1)
+{
+while (*t++)
+{
+i++;
+}
+}
+else
+s1 = "";
+t = s2;
+if (s2)
+{
+while (t++)
+{
+j++;
+}
+}
+else
+s2 = "";
+m = malloc(i + j + 1);
+if (!m)
+return (NULL);
+t = m;
+while (*s1)
+{
+*t++ = *s1++;
+}
+while (*s2)
+{
+*t++ = *s2++;
+}
+*t = 0;
+return (t);
 }
