24cd8d946c685d155d80f105076b75046bf908f2
HajarElMannani <laso125@hotmail.com>
2023-11-02 10:07:09 -0700

    Fix error

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,14 +10,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
-void *ptr;
+char *ptr;
 i = 0;
 if (nmemb == 0 || size == 0)
 return (NULL);
 ptr = malloc(nmemb * size);
 if (ptr == NULL)
 return (NULL);
-while (i < nmemb)
+while (i < (nmemb * size))
 {
 ptr[i] = 0;
 i++;
