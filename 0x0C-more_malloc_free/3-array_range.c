24676a310f285da0426cbd23fe003b1f4d8f4f42
HajarElMannani <laso125@hotmail.com>
2023-11-02 10:56:38 -0700

    Fix error

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,14 +9,15 @@
 int *array_range(int min, int max)
 {
 int *arr;
-int i;
+int i, n;
+n = max - min + 1;
 if (min > max)
 return (NULL);
-arr = malloc((max - min + 1) * sizeof(int));
+arr = malloc(n * sizeof(int));
 if (arr == NULL)
 return (NULL);
 arr[0] = min;
-for (i = 1; i < (max - min + 1); i++)
+for (i = 1; i < n; i++)
 {
 arr[i] = min++;
 }
