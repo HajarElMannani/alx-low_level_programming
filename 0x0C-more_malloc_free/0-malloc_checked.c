52ba4c060f67fb48b7b163525cc7e59143268f67
HajarElMannani <laso125@hotmail.com>
2023-11-02 05:34:54 -0700

    First commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *malloc_checked - function that allocates memory using malloc
+ *@b:size of memory to allocate
+ *Return: pointer to the allocated memory
+ *if faillure, terminate  with status value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+{
+_putchar('9');
+_putchar('8');
+_putchar('\n');
+exit(1);
+}
+else
+return (ptr);
+}
