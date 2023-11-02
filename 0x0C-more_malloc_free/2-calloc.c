31d9c717c26f6fc41648d48c281211d328798119
HajarElMannani <laso125@hotmail.com>
2023-11-02 09:16:33 -0700

    First commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,14 +9,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int *ptr, *s;
+unsigned int i;  
+int *ptr;
+int *s;
+i = 0; 
 if (nmemb == 0 || size == 0)
 return (NULL);
 ptr = malloc(nmemb * size);
 if (ptr == NULL)
 return (NULL);
 s = ptr;
-while (*s)
-*s++ = 0;
+while (i < size)
+s[i++] = 0;
 return (ptr);
 } 
