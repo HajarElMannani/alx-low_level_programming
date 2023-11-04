30540c96e7c0f7e8c62798e8fa4523368517020d
HajarElMannani <laso125@hotmail.com>
2023-11-04 10:42:30 -0700

    First commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,9 +6,9 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 #endif /*DG_H*/
