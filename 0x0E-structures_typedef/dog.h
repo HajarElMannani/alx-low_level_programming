53eebbc6b79173abe5336d343da052fea4d97e81
HajarElMannani <laso125@hotmail.com>
2023-11-04 10:46:12 -0700

    Fix error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,5 @@
 #ifndef DG_H
 #define DG_H
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 /**
  *struct dog: structure containing name, age and owner
  */
