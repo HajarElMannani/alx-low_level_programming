f90659924a2cacafa1272a012878fcb218afa64d
HajarElMannani <laso125@hotmail.com>
2023-11-04 14:11:02 -0700

    First commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *new_dog - function that creates a new dog
+ *@name: Name of dog
+ *@age: Age of dog
+ *@owner: Owner of dog
+ *Return: new dog data
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+  dog_t new_dog;
+  if (new_dog == NULL)
+    return (NULL);
+  
+  
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *free_dog - function that frees dogs
+ *@d: dog's data
+ *Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+free(d);
+}
