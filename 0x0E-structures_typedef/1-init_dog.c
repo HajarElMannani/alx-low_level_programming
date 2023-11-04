54283ea46e8ea8ff168b34d3c42fa6bf84c60703
HajarElMannani <laso125@hotmail.com>
2023-11-04 11:38:08 -0700

    Fix error

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,9 +10,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog *d;  
+d = malloc(sizeof(struct dog));
+if (d == NULL)
+return (NULL);
 (*d).name = name;
 (*d).age = age;
 (*d).owner = owner;
 return (d);
+free(d);
 }
