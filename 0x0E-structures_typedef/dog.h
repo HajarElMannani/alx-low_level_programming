f1390561824c79ef6bce2b152a1760ed55211c82
HajarElMannani <laso125@hotmail.com>
2023-11-04 10:45:03 -0700

    First commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,6 +4,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+/**
+ *struct dog: structure containing name, age and owner
+ */
 struct dog
 {
 char *name;
