6cd067beef24ab50cda6ad454ced07405e6f94e5
HajarElMannani <laso125@hotmail.com>
2023-11-08 07:32:04 -0800

    First commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 int i;
 i = 0;
 if (array == NULL || size <= 0 || action == NULL)
-return;
+return (NULL);
 while (i < size)
 {
 (*action)(array[i]);
