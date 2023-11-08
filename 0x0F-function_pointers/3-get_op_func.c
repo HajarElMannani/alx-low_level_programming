3dcaf91b2b63a4c7d2b66fb64bc6c22380c8ada2
HajarElMannani <laso125@hotmail.com>
2023-11-08 12:55:13 -0800

    Fix error

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int i;
 i = 0;
 while (ops[i].op)
 {
-if (ops[i].op == *s)
+if (*ops[i].op == *s)
 return (ops[i].f);
 i++;
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
@@ -10,23 +11,22 @@
 int main(int argc, char *argv[])
 {
 int num;
-if (argc > 4)
+if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-if (argv[2] != '+' || argv[2] != '-' || argv[2] != '*'
-|| argv[2] != '/' || argv[2] != '%')
+if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
-if ((argv[2] == '/' || argv[2] == '%') && atoi(argv[3]) == 0)
+if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
 {
 printf("Error\n");
 exit(100);
 }
-num =  get_op_func(*argv[2])(atoi(argv[1]), atoi(argv[3]));
+ num =  get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 printf("%d\n", num);
 return (0);
 }
