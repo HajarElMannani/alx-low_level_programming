02420b811ba67dc55d26dc8e9a9f1cf21c8bbb60
HajarElMannani <laso125@hotmail.com>
2023-11-09 06:08:02 -0800

    First commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- *print_all - function that prints anything
+ *print_strings - function that prints anything
  *@separator: string of chracters between numbers
  *@n: number of argument
  *Return: Nothing
@@ -22,7 +22,7 @@ else
 ptr1 = (char *)separator;
 if (n > 0)
 printf("%s", va_arg(prstr, char *));
-for(i = 1; i < n ; i++)
+for (i = 1; i < n ; i++)
 {
 ptr2 = va_arg(prstr, char *);
 if (ptr2 == NULL)
