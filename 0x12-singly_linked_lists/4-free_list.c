19811d2f969836b24f278e2f68370712661a1e58
HajarElMannani <laso125@hotmail.com>
2023-11-16 10:43:17 -0800

    Fix Betty style

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
  *free_list - function that frees a list_t list
- *@head - a list
+ *@head: a list
  *Return: Nothing
  */
 void free_list(list_t *head)
 {
 list_t *temp;
-while (head->next != NULL)
+while (head != NULL)
 {
 temp = head->next;
 free(head->str);
 free(head);
-head->next = temp;
+head = temp;
 }
 }
