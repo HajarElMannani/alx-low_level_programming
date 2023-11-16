d1251ce452db054d6c0ab9899db49c9b803f6742
HajarElMannani <laso125@hotmail.com>
2023-11-16 11:17:50 -0800

    First commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *before_main - print string before excuting main
+ *Return: nothing
+ */
+void before_main(void) __attribute__((constructor));
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
