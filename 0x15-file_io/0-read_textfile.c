4030cfb5bbd5877167a038a23fcb64b44895657e
HajarElMannani <laso125@hotmail.com>
2023-11-27 04:07:37 -0800

    add new conditiion

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,6 +11,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 int p;
 ssize_t len, wr;
 char *buffer;
+if (filename == NULL)
+return (0);
 buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
 return (0);
