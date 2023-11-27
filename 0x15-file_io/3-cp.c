0aa880d9dea5f164895aedfa6043367cc9bb25a5
HajarElMannani <laso125@hotmail.com>
2023-11-27 12:42:36 -0800

    Fix betty style

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,14 +16,29 @@ exit(99);
 return (buff);
 }
 /**
+ *closef - close file
+ *@file: file descriptor
+ *Return: Nothing
+ */
+void closef(int file)
+{
+int cl;
+cl = close(file);
+if (cl == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close %d\n", file);
+exit(100);
+}
+}
+/**
  *main - copy content of file into another
  *@argc: Number of arguments
  *@argv: arguments
  *Return: 0 when success
  */
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-int file1, file2, len, wr, cl1, cl2;
+int file1, file2, len, wr;
 char *buffer;
 if (argc != 3)
 {
@@ -32,45 +47,26 @@ exit(97);
 }
 buffer = buff(argv[2]);
 file1 = open(argv[1], O_RDONLY);
-if (file1 == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
 file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (file2 == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file1);
-exit(99);
-}
-do{
+do {
 len = read(file1, buffer, 1024);
-if (len == -1)
+if (len == -1 || file1 == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 free(buffer);
 exit(98);
 }
 wr = write(file2, buffer, len);
-if (wr == -1 || wr != len)
+if (file2 == -1 || wr == -1 || wr != len)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(file1);
 free(buffer);
 exit(99);
 }
-}while (len > 0);
-cl1 = close(file1);
-cl2 = close(file2);
-if (cl1 == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close %d\n",file1);
-exit(100);
-}
-if (cl2 == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close %d\n", file2);
-exit(100);
-}
+} while (len > 0);
+free(buffer);
+closef(file1);
+closef(file2);
 return (0);
 }
