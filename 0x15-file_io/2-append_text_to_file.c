5ad0cf151128b97fe5434bc2a408c5c766c66453
HajarElMannani <laso125@hotmail.com>
2023-11-27 07:30:00 -0800

    first commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,28 +6,27 @@
  *@text_content: text to append
  *Return: 1 on success and -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-  int p, len, wr;
-  len = 0;
-  if (filename == NULL)
-    return (-1);
-  p = open(filename, O_APPEND | O_ RDWR);
-  if (p == -1)
-    return (-1);
-  if (text_content == NULL)
-    return (1);
-  if (text_content != NULL)
-    {
-      while (text_content[len] != '\0')
-	{
-	  len++;
-	}
-      wr = write(p, text_content, len);
-    }
-  if (wr == -1 || wr != len)
-    return (-1);
-  close(p);
-  return (1);
+int p, len, wr;
+len = 0;
+if (filename == NULL)
+return (-1);
+p = open(filename, O_WRONLY | O_APPEND);
+if (p == -1)
+return (-1);
+if (text_content == NULL)
+return (1);
+if (text_content != NULL)
+{
+while (text_content[len] != '\0')
+{
+len++;
+}
+}
+wr = write(p, text_content, len);
+if (wr == -1 || wr != len)
+return (-1);
+close(p);
+return (1);
 }
