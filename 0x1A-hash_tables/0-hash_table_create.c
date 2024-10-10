671a4fdcc1b4001dc2640f8e899958268a022863
HajarElMannani <laso125@hotmail.com>
2024-10-10 14:55:07 -0700

    first commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size of the array
+ * Return: address of the hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *hash_t;
+hash_t = malloc(sizeof(hash_node_t *) * size);
+if (hash_t == NULL)
+return (NULL);
+return (hash_t);
+}
