452ad0e7cf888dc64058297a137b2ccf90f8a713
HajarElMannani <laso125@hotmail.com>
2024-10-10 15:27:16 -0700

    add array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *hash_t;
-hash_t = malloc(sizeof(hash_node_t *) * size);
+hash_node_t **array;
+unsigned int i;
+hash_t = malloc(sizeof(hash_table_t));
 if (hash_t == NULL)
 return (NULL);
+array = malloc(sizeof(hash_node_t *) * size);
+if (array == NULL)
+return (NULL);
+hash_t->size = size;
+for (i = 0; i < size; i++)
+{
+array[i] = NULL;
+}
+hash_t->array = array;
 return (hash_t);
 }
