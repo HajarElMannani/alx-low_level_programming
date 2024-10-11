560ec5d61ee7697d9b2113144a8f44cde5b10ad3
HajarElMannani <laso125@hotmail.com>
2024-10-11 10:36:54 -0700

    change condition

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,18 +14,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 unsigned long int index;
 hash_node_t *new_node;
 
-if (ht == NULL || key == NULL || value == NULL)
+if (ht == NULL || strcmp(key, "") == 0 || key == NULL)
 return (0);
 
 index = key_index((const unsigned char *)key, ht->size);
-if (ht->array[index] == NULL)
+if (ht->array[index] && strcmp(ht->array[index]->key, key) == 0)
 {
-ht->array[index]->key = strdup(key);
+free(ht->array[index]->value);
 ht->array[index]->value = strdup(value);
 return (1);
 }
-else
-{
 new_node = malloc(sizeof(hash_node_t));
 if (new_node == NULL)
 return (0);
@@ -34,6 +32,5 @@ new_node->value = strdup(value);
 new_node->next = ht->array[index];
 ht->array[index] = new_node;
 return (1);
-}
 }
 
