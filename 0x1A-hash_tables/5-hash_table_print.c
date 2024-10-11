32a0c89f8817f31cb72205e80e6fe214aab3a6bf
HajarElMannani <laso125@hotmail.com>
2024-10-11 13:59:36 -0700

    change output format

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,7 +10,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 unsigned long int i;
-short commaf = 0;
+unsigned char commaf = 0;
 if (ht == NULL)
 return;
 printf("{");
@@ -20,7 +20,7 @@ while (ht->array[i] != NULL)
 {
 if (commaf == 1)
 printf(", ");
-printf("\'%s\' : \'%s\'", ht->array[i]->key, ht->array[i]->value);
+printf("\'%s\': \'%s\'", ht->array[i]->key, ht->array[i]->value);
 ht->array[i] = ht->array[i]->next;
 commaf = 1;
 }
